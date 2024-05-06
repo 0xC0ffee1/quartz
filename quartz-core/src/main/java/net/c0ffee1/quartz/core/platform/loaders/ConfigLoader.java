7bd67816e73556fa54da3ea0f9b85e67c54f6ad5
0xC0ffee1 <samthrw@proton.me>
2024-05-06 18:48:52 +0300

    fix

--- quartz-core/src/main/java/net/c0ffee1/quartz/core/platform/loaders/ConfigLoader.java
+++ quartz-core/src/main/java/net/c0ffee1/quartz/core/platform/loaders/ConfigLoader.java
@@ -1,14 +1,10 @@
 package net.c0ffee1.quartz.core.platform.loaders;
 
-import net.c0ffee1.quartz.core.annotations.Command;
-import net.c0ffee1.quartz.core.annotations.Component;
+
 import net.c0ffee1.quartz.core.annotations.Config;
-import net.c0ffee1.quartz.core.annotations.Service;
+
 import org.reflections.Reflections;
 
-import java.lang.annotation.Annotation;
-import java.util.HashSet;
-import java.util.Set;
 import java.util.function.Consumer;
 
 public class ConfigLoader extends AbstractLoader<Class<?>> {
